cd3cbe0f7bd04cd5bf303ca8384cb2aa9974f5c8
Renan Alves Medina <alvesrenansantos@hotmail.com>
2025-04-09 16:39:45 -0300

    update doc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,7 +64,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.png',
     navbar: {
       title: 'SyncTrade AI',
       logo: {
